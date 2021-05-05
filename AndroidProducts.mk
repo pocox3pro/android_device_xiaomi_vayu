ae0add3e7787b59ff14354e2cfb81178addf4c6c
rahul9999xda <rahul9999xda@gmail.com>
2021-05-05 04:18:54 +0000

    vayu: Derpify
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_vayu.mk
+    $(LOCAL_DIR)/derp_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_vayu-user \
-    fluid_vayu-userdebug \
-    fluid_vayu-eng
+    derp_vayu-user \
+    derp_vayu-userdebug \
+    derp_vayu-eng
--- derp_vayu.mk
+++ derp_vayu.mk
@@ -7,24 +7,17 @@
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit some common Fluid stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Official-ify
+DERP_BUILDTYPE := Official
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_vayu
+PRODUCT_NAME := derp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Fluid flags
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.fluid.maintainer=Foxtrot47 \
-	ro.fluid.cpu=SDM860
-
-# GApps
-TARGET_INCLUDE_GAPPS := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_GOOGLE_RECORDER := true
