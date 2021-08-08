c7d419842b105803a7574be2ad8947c38c8c7d47
Lunarixus <Nathanb644@hotmail.co.uk>
2021-08-08 09:01:49 +0000

    vayu: Remove some Gapps packages at build time
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,10 @@ PRODUCT_COPY_FILES += \
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
